ca21b8bfbff727fd107f9e5983bc0e42363d5d69
ConstantaNF <162187256+ConstantaNF@users.noreply.github.com>
2024-05-22 13:21:28 +0300

    Update cronscript.sh

--- cronscript.sh
+++ cronscript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Контроль активности скрипта
-if [ -f ~/bash/script.pid ]; then
+if [ -f /vagrant/script.pid ]; then
   echo "Скрипт в данный момент запущен. Параллельный запуск не воможен"
   exit 1
 fi
-touch ~/bash/script.pid
+touch /vagrant/script.pid
 
 # Объявление функций
 function sortip {
@@ -41,14 +41,14 @@ string >> access_report
 
 # Сортировка по IP:
 # объявление переменных
-lastd=$(cat ~/bash/lastrun)
+lastd=$(cat /vagrant/lastrun)
 nowd=$(LANG=C date +%d/%b/%y:%H:%M:%S)
-lastt=$(cat ~/bash/lastrun | sed 's/..........//')
+lastt=$(cat /vagrant/lastrun | sed 's/..........//')
 nowt=$(date "+%T")
-logt=$(cat ~/bash/access.log | awk '{print $4}' | sed 's/.............//')
+logt=$(cat /vagrant/access.log | awk '{print $4}' | sed 's/.............//')
 
 # сортировка
-cat ~/bash/access.log | sed 's/\[//g' | awk -v last_run="$lastd" -v Now="$nowd" -v lastt_run="$lastt" -v Nowt="$nowt" -v Logt="$logt" \
+cat /vagrant/access.log | sed 's/\[//g' | awk -v last_run="$lastd" -v Now="$nowd" -v lastt_run="$lastt" -v Nowt="$nowt" -v Logt="$logt" \
 '$4 > last_run && $4 < Now && "$Logt" > lastt_run && "$Logt" < Nowt {print $1}' | sort -g | uniq -c | sort -gr | sed 's/^[ ^t]*//' >> access_report
 
 string >> access_report
@@ -57,7 +57,7 @@ line >> access_report
 string >> access_report
 
 # Сортировка по URL
-cat ~/bash/access.log | awk '{print $11}' | sort -gr | uniq -c | sort -gr | sed 's/^[ ^t]*//' >> access_report
+cat /vagrant/access.log | awk '{print $11}' | sort -gr | uniq -c | sort -gr | sed 's/^[ ^t]*//' >> access_report
 
 string >> access_report
 allcode >> access_report
@@ -65,7 +65,7 @@ line >> access_report
 string >> access_report
 
 # Список всех кодов HTTP ответа
-cat ~/bash/access.log | awk '{print $9}' | sort -gr | uniq -c | sort -gr | sed 's/^[ ^t]*//' >> access_report
+cat /vagrant/access.log | awk '{print $9}' | sort -gr | uniq -c | sort -gr | sed 's/^[ ^t]*//' >> access_report
 
 string >> access_report
 err >> access_report
@@ -73,12 +73,12 @@ line >> access_report
 string >> access_report
 
 # Строки с ошибкой сервера/приложения
-cat ~/bash/access.log | awk '$9 ~ /400|403|405|499|500/' >> access_report
+cat /vagrant/access.log | awk '$9 ~ /400|403|405|499|500/' >> access_report
 
 # Контроль времени запуска скрипта
 LANG=C date +%d/%b/%y:%H:%M:%S > lastrun
 
 # Контроль активности скрипта
-rm ~/bash/script.pid
+rm /vagrant/script.pid
 
 
